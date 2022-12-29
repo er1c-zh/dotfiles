da394eded414dcca8732013c91fa449f9a79fe77
eric <ericzhao96@hotmail.com>
2022-12-29 09:28:30 +0800

    feat: multi dictionary

--- tampermonkey/export-collins.js
+++ tampermonkey/export-collins.js
@@ -14,45 +14,63 @@
 
 (function() {
     'use strict';
-    let first = document.getElementsByClassName("title_frequency_container")[0];
-    if (first) {
+    let video_div = document.getElementById("videos");
+    if (video_div) {
+        video_div.style.display = 'none';
+    }
+    Array.from(document.getElementsByClassName("dictlink")).forEach(function(dic) {
+        if (!dic) {
+            return;
+        }
+        let title_container = dic.getElementsByClassName("title_frequency_container")[0];
         let btn = document.createElement("BUTTON");
         btn.textContent = "export";
         btn.type = "button";
         btn.onclick = function() {
-            export_as_json(this);
+            export_as_json(dic, this);
         };
-        first.appendChild(btn);
-    }
+        title_container.appendChild(btn);
+    });
 })();
 
 function elog(s) {
     GM_log("eric: " + s);
 }
 
-function export_as_json(btn) {
-    let first = document.getElementsByClassName("dictionaries dictionary")[0].children[0];
-    let word = first.getElementsByClassName("orth")[0].innerHTML;
+function export_as_json(dic, btn) {
+    let word = dic.getElementsByClassName("orth")[0].innerHTML;
     let ipa = function(){
         var _ipa = "";
-        Array.from(first.getElementsByClassName("pron")[0].childNodes).slice(0,-1).forEach(function(ele) {
+        Array.from(dic.getElementsByClassName("pron")[0].childNodes).forEach(function(ele) {
+            if (ele.classList && ele.classList.contains("ptr")) {
+                return;
+            }
             _ipa += ele.outerHTML ? ele.outerHTML : ele.textContent;
         });
         return _ipa;
     }();
     var meaning = [];
-    Array.from(first.getElementsByClassName("hom")).forEach(function(ele){
+    let def_container = dic.getElementsByClassName("content definitions")[0];
+    Array.from(def_container.getElementsByClassName("hom")).forEach(function(ele){
         var mean = "";
         let pos = ele.getElementsByClassName("pos")[0];
         if (pos) {
             mean += "[" + pos.innerHTML + "]";
         }
-        let def = ele.getElementsByClassName("def")[0];
-        if (!def) {
-            return;
-        }
-        Array.from(def.childNodes).forEach(function(ele){
-            mean += ele.innerHTML ? ele.innerHTML : ele.textContent;
+        var serial = 1;
+        Array.from(ele.getElementsByClassName("def")).forEach(function(def){
+            if (!def) {
+                return;
+            }
+            if (serial > 1) {
+                mean += "<br>";
+            }
+            mean += serial + ". ";
+            serial += 1
+
+            Array.from(def.childNodes).forEach(function(ele){
+                mean += (ele.innerHTML ? ele.innerHTML : ele.textContent);
+            });
         });
         meaning.push(mean);
     });
@@ -65,3 +83,4 @@ function export_as_json(btn) {
     GM_setClipboard(j);
     btn.textContent = "done";
 }
+
